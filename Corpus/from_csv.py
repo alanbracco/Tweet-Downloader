6f7880b4bc9c79c969dacafbb0ba60746f2456b1
Alan Bracco <alan.bracco@gmail.com>
2018-02-06 07:44:11 -0300

    Fixed from_csv

--- Corpus/from_csv.py
+++ Corpus/from_csv.py
@@ -9,6 +9,7 @@ Options:
   -o <filename>     Output file.
   -h --help         Show this screen.
 """
+import os
 import csv
 import sys
 from docopt import docopt
@@ -22,19 +23,22 @@ if __name__ == '__main__':
         if not os.path.exists(input_file):
             sys.exit("File does not exist")
 
-        with open(input_file, "r") as infile:
-            reader = csv.reader(infile)
-            with open(input_file.replace(".csv", ".txt"), "w") as outfile:
-                i = 0
-                for row in reader:
-                    if i == 0:
-                        i += 1
-                        continue
-                    id = row[0]
-                    text = row[1]
-                    corrections = row[2].split(' | ')
-                    insult = row[3]
-                    outfile.write(id + '\t' + text + '\n')
-                    if corrections != ['']:
-                        for correction in corrections:
-                            outfile.write('\t' + correction + '\n')
+    output_file = opts['-o']
+    if not output_file:
+        output_file = input_file.replace(".csv", ".txt")
+
+    with open(input_file, "r") as infile:
+        reader = csv.reader(infile)
+        with open(output_file, "w") as outfile:
+            i = 0
+            for row in reader:
+                if i == 0:
+                    i += 1
+                    continue
+                id = row[0]
+                text = row[1]
+                corrections = row[2].split(' | ')
+                outfile.write(id + '\t' + text + '\n')
+                if corrections != ['']:
+                    for correction in corrections:
+                        outfile.write('\t' + correction + '\n')
